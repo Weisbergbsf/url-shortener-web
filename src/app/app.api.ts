16bb9c90fbb486d012dbd9d491aadda37a03868f
Weisberg Barros <weisbergbsf@gmail.com>
2020-06-10 14:24:49 -0300

    Created file app.api (URL_API)

---
+++ src/app/app.api.ts
@@ -0,0 +1,13 @@
+import { environment } from "./../environments/environment";
+
+let URL_API = "http://localhost:8080/api/url"
+
+if (environment.production) {
+    URL_API = "https://url-shortenerr-api.herokuapp.com/api/url"
+    console.log("Running in mode prod: ", URL_API)
+} else {
+    URL_API = "http://localhost:8080/api/url"
+    console.log("Running in mode dev: ", URL_API)
+}
+
+export { URL_API }
